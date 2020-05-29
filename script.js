5545de0b7b002ca9b0afb7b1e8bf23d240cf835a
bikramshankhar <56664732+bikramshankhar@users.noreply.github.com>
2020-05-29 12:05:47 -0500

    work done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$( '#topheader .navbar-nav a' ).on( 'click', function () {
-	$( '#topheader .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
-	$( this ).parent( 'li' ).addClass( 'active' );
-});
+// $( '#topheader .navbar-nav a' ).on( 'click', function () {
+// 	$( '#topheader .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
+// 	$( this ).parent( 'li' ).addClass( 'active' );
+// });
